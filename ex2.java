b7cf0f117cff10d301385b6eab443dfe7dcefd46
venkyslm <143099087+venkyslm@users.noreply.github.com>
2023-08-24 20:18:07 +0530

    Update ex2.java with print statement
    Update ex2.java with the print statement for the first time from GitHub website.

--- ex2.java
+++ ex2.java
@@ -3,6 +3,6 @@ public static void main(string arg[]){
 
 System.out.println("2nd commit: first modification to the 2nd commit")
 
-
+System.out.println("Added this line from GitHub website for first time")
 }
 }
