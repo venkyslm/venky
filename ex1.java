05d6640a6effdfbd7cd984bf5096fbe8bea5a48d
venkyslm <techpasdo@gmail.com>
2023-08-24 19:52:25 +0530

    commit from new author venkyslm2

--- ex1.java
+++ ex1.java
@@ -1,9 +1,9 @@
 public class ex1{
-public static void main(string arg[]){
+public static void main(String arg[]){
 
-System.out.println("2nd commit: first modification to the 2nd commit")
+System.out.println("2nd commit: first modification to the 2nd commit");
 
-System.out.println("making print command by venkyslm")
+System.out.println("making print command by venkyslm");
 
 
 }
