99ee9cf4be03c11b1ddcc79e9fe99f39e602d52c
venkyslm <143099087+venkyslm@users.noreply.github.com>
2023-08-25 07:12:23 +0530

    Updated ex1 file from devteam1
    Updated ex1 file from devteam1 from GitHub website

--- ex1.java
+++ ex1.java
@@ -5,6 +5,6 @@ System.out.println("2nd commit: first modification to the 2nd commit");
 
 System.out.println("making print command by venkyslm");
 
-
+System.out.println("Added this line of code by devteam1 from GitHub website");
 }
 }
