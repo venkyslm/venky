b64358c0f08da1726f1215263aa57bc0cbbe7b83
venkyslm <143099087+venkyslm@users.noreply.github.com>
2023-08-26 06:55:40 +0530

    Added this line from techteam1 from GitHub website

--- ex1.java
+++ ex1.java
@@ -6,5 +6,7 @@ System.out.println("2nd commit: first modification to the 2nd commit");
 System.out.println("making print command by venkyslm");
 
 System.out.println("Added this line of code by devteam1 from GitHub website");
+
+System.out.println("Added this line from techteam1 from GitHub website");
 }
 }
